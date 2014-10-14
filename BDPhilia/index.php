c619e365d9861cc247af46bf7bfe31762e6ba6fa
Administrateur <Administrateur@192.168.11.1>
2014-10-14 12:11:14 +0200

    Exercice 3

--- BDPhilia/index.php
+++ BDPhilia/index.php
@@ -1,26 +1,24 @@
-       <?php
-        $xmlVersion = '1.0';
-        $xmlEncoding = 'UTF-8';
-        $lang = 'fr';
-        $projet = 'BDPhilia';
-        $proprietaire = 'Inexine';
-        $auteur = 'Pierre';
-        $css = 'css.css';
-        $titre = 'Bienvenue sur ' . $projet;
-        $footer = '&copy; ' . $auteur . " 2014";
-        ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-      
+<?php
+$xmlVersion = '1.0';
+$xmlEncoding = 'UTF-8';
+$lang = 'fr';
+$projet = 'BDPhilia';
+$proprietaire = 'Inexine';
+$auteur = 'Pierre';
+$css = 'css.css';
+$titre = 'Bienvenue sur ' . $projet;
+$footer = '&copy; ' . $auteur . " 2014";
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">   
     <head>
         <meta charset="UTF-8">
         <meta name="author" content="<?php echo $auteur; ?>">
         <meta name="content-type" content="='xhtml'">
         <link href="<?php echo $css; ?>" rel="stylesheet" type="text/css" media="screen"/>
-        <title>BDPHILIA</title>
+        <title><?php echo $projet; ?></title>
     </head>
     <body>
- 
-        <div id="Btop"> 
+         <div id="Btop"> 
             <h1><?php echo $titre ?></h1>
             <div id=""Topmenu">
                  <ul>
@@ -32,7 +30,7 @@
             </div>
             <div id="Bmiddle">
                 <div id="Bbottom">
-                    <?php
+                    <?php 
                     echo $footer
                     ?>
                 </div>
